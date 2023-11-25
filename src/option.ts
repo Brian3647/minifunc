0db48c5f5ca3065d0754894ea9e121d86b1ac9fb
Brian3647 <victoris3647@gmail.com>
2023-11-25 12:09:09 +0100

    docs(jsdoc): Better docs

--- src/option.ts
+++ src/option.ts
@@ -1,27 +1,20 @@
 /**
- * Option type, either Some(T) or None. Useful alternative to null/undefined.
- * @param T Type of the value
- * @example
- * const some = Some(1);
- * const none = None();
- * some.isSome(); // true
- * some.isNone(); // false
+ * Option type, either Some(T) or None. Safer alternative to null/undefined.
  *
- * const other = Option.fromNullable(null); // None value
- * other.isSome(); // false
- * other.unwrap(); // throws Error
- * @note None() needs to be a function call for type safety. If you don't care about that,
+ * Note: `None()` needs to be a function call for type safety. If you don't care about that,
  * you can initialize None as `const None = Option.None();` and use it as `None` instead of `None()`.
+ * @template T Type of the value
  */
 export class Option<T> {
 	/**
 	 * The value of the Option, if it is Some.
-	 * @note Do not use this directly.
+	 * @private
 	 */
 	private value: T | undefined;
 
 	/**
 	 * Not meant to be used directly, use Some() or None() instead.
+	 * @private
 	 */
 	private constructor(value?: T | undefined) {
 		this.value = value;
@@ -29,94 +22,114 @@ export class Option<T> {
 
 	/**
 	 * A `Some` value.
+	 * @returns {Option<T>}
 	 */
-	static Some<T>(value: T) {
+	static Some<T>(value: T): Option<T> {
 		return new Option(value);
 	}
 
 	/**
 	 * A `None` value, nothing.
+	 * @returns {Option<T>}
 	 */
-	static None<T>() {
+	static None<T>(): Option<T> {
 		return new Option<T>();
 	}
 
 	/**
 	 * Creates a new Option from a nullable value.
-	 * @example
-	 * Option.fromNullable(1); // Some(1)
-	 * Option.fromNullable(null); // None
-	 * Option.fromNullable(undefined); // None
+	 * @param {T?} value The value to convert to an Option.
+	 * @returns {Option<T>} `Some(value)` if the value is not null or undefined, `None` otherwise.
 	 */
-	static fromNullable<T>(value: T | null | undefined) {
+	static fromNullable<T>(value: T | null | undefined): Option<T> {
 		return value === null || value === undefined
 			? Option.None<T>()
 			: Option.Some(value);
 	}
 
 	/**
-	 * @returns true if the Option is Some, false otherwise.
+	 * Checks if the value is Some.
+	 * @returns {boolean} true if the Option is Some, false otherwise.
 	 */
-	isSome() {
+	isSome(): boolean {
 		return this.value !== undefined;
 	}
 
 	/**
-	 * @returns true if the Option is None, false otherwise.
+	 * Checks if the value is None.
+	 * @returns {boolean} true if the Option is None, false otherwise.
 	 */
-	isNone() {
+	isNone(): boolean {
 		return !this.isSome();
 	}
 
 	/**
-	 * @returns the value if the Option is Some, throws an Error otherwise.
+	 * Unwraps the value if the Option is Some, throws an Error otherwise.
+	 * @returns {T} the value if the Option is Some.
+	 * @throws if the Option is None.
 	 */
-	unwrap() {
+	unwrap(): T {
 		if (this.value === null) {
 			throw new Error('Unwrapped None');
 		}
 
-		return this.value;
-	}
-
-	/**
-	 * @returns the value as T, but not sure if it's Some or not.
-	 * @note This is an unchecked version of `unwrap()`. Use this only if you
-	 * are 100% sure that the Option is Some. If you aren't, this completely defeats
-	 * the purpose of using Option in the first place.
-	 */
-	unwrapUnchecked() {
 		return this.value as T;
 	}
 
 	/**
-	 * @returns the value if the Option is Some, returns undefined otherwise.
-	 * @note Do NOT use function calls on `defaultValue`. That will call functions
-	 * even if the Option is Some. Use [`unwrapOrElse`] instead.
+	 * Note: This is an unchecked version of `unwrap()`. Use this only if you
+	 * are 100% sure that the Option is Some. If you aren't, this completely defeats
+	 * the purpose of using Option in the first place.
+	 * @returns {T} the value as T, but not sure if it's Some or not.
 	 */
-	unwrapOr(defaultValue: T) {
+	unwrapUnchecked(): T {
+		return this.value as T;
+	}
+
+	/**
+	 * Do NOT use function calls on `defaultValue`. That will call functions
+	 * even if the Option is Some. Use `unwrapOrElse` instead.
+	 *
+	 * @returns {T} the value if the Option is Some, returns undefined otherwise.
+	 * @param {T} defaultValue The value to return if the Option is `None`.
+	 * @see unwrapOrElse
+	 */
+	unwrapOr(defaultValue: T): T {
 		return this.value || defaultValue;
 	}
 
 	/**
-	 * @returns the value if the Option is Some, returns the result of the function otherwise.
+	 * @param {function(): T} fn The function to call if the Option is None.
+	 * @returns {T} the value if the Option is Some, returns the result of the function otherwise.
 	 */
-	unwrapOrElse(fn: () => T) {
+	unwrapOrElse(fn: () => T): T {
 		return this.value || fn();
 	}
 
 	/**
 	 * Maps an `Option<T>` to `Option<U>` by applying a function to a contained value,
 	 * if it's Some.
-	 * @note This does not mutate the Option, it returns a new one. Use `mapSelf` if
+	 *
+	 * Note: This does not mutate the Option, it returns a new one. Use `mapSelf` if
 	 * you want to mutate the Option.
+	 *
+	 * @template U Type of the new value
+	 * @param {function(T): U} fn The function to apply to the value.
+	 * @returns {Option<U>} `Some(fn(value))` if the Option is Some, `None` otherwise.
 	 */
-	map<U>(fn: (value: T) => U) {
+	map<U>(fn: (value: T) => U): Option<U> {
 		return this.value ? Option.Some(fn(this.value)) : Option.None<U>();
 	}
 }
 
+/**
+ * @alias Option.Some
+ */
 export const Some = Option.Some;
+
+/**
+ * @alias Option.None
+ */
 export const None = Option.None;
 
 // Type aliases for convenience
--- src/result.ts
+++ src/result.ts
@@ -1,13 +1,24 @@
+/**
+ * Result type, representing either an Ok value or an error.
+ * @template T Type of the Ok value
+ * @template E Type of the error
+ */
 export class Result<T, E> {
 	/**
 	 * The value of the Result, either the Ok value or an error.
-	 * @note Do not use this directly.
+	 * @private
 	 */
 	private readonly value: T | E;
+
+	/**
+	 * Indicates whether the Result is an error.
+	 * @private
+	 */
 	private readonly isError: boolean;
 
 	/**
 	 * Not meant to be used directly, use Ok() or Err() instead.
+	 * @private
 	 */
 	private constructor(value: T | E, isError: boolean) {
 		this.value = value;
@@ -15,37 +26,45 @@ export class Result<T, E> {
 	}
 
 	/**
-	 * An `Ok` value.
+	 * Creates an `Ok` Result.
+	 * @param {T} value The Ok value.
+	 * @returns {Result<T, E>} An `Ok` Result.
 	 */
-	static Ok<T, E>(value: T) {
+	static Ok<T, E>(value: T): Result<T, E> {
 		return new Result<T, E>(value, false);
 	}
 
 	/**
-	 * An `Err` value, an error.
+	 * Creates an `Err` Result.
+	 * @param {E} value The error value.
+	 * @returns {Result<T, E>} An `Err` Result.
 	 */
-	static Err<T, E>(value: E) {
+	static Err<T, E>(value: E): Result<T, E> {
 		return new Result<T, E>(value, true);
 	}
 
 	/**
-	 * @returns true if the Result is Ok, false otherwise.
+	 * Checks if the Result is Ok.
+	 * @returns {boolean} true if the Result is Ok, false otherwise.
 	 */
-	isOk() {
+	isOk(): boolean {
 		return !this.isError;
 	}
 
 	/**
-	 * @returns true if the Result is Err, false otherwise.
+	 * Checks if the Result is Err.
+	 * @returns {boolean} true if the Result is Err, false otherwise.
 	 */
-	isErr() {
+	isErr(): boolean {
 		return this.isError;
 	}
 
 	/**
-	 * @returns the value if the Result is Ok, throws an Error otherwise.
+	 * Unwraps the Ok value.
+	 * @returns {T} The Ok value.
+	 * @throws {Error} If called on an Err value.
 	 */
-	unwrap() {
+	unwrap(): T {
 		if (this.isOk()) {
 			return this.value as T;
 		}
@@ -56,9 +75,11 @@ export class Result<T, E> {
 	}
 
 	/**
-	 * @returns the error if the Result is Err, throws an Error otherwise.
+	 * Unwraps the error value.
+	 * @returns {E} The error value.
+	 * @throws {Error} If called on an Ok value.
 	 */
-	unwrapErr() {
+	unwrapErr(): E {
 		if (this.isErr()) {
 			return this.value as E;
 		}
@@ -69,47 +90,61 @@ export class Result<T, E> {
 	}
 
 	/**
-	 * @returns the value if the Result is Ok, or a default value.
-	 */ /**
-	 * Maps an Option<T> to Option<U> by applying a function to a contained value, if it's Some.
+	 * Unwraps the Ok value or returns a default value. Don't use this if
+	 * the default value is expensive to compute, use unwrapOrElse instead for that.
+	 * @param {T} defaultValue The default value.
+	 * @returns {T} The Ok value or the default value.
+	 * @see unwrapOrElse
 	 */
-
-	unwrapOr(defaultValue: T) {
+	unwrapOr(defaultValue: T): T {
 		return this.isOk() ? (this.value as T) : defaultValue;
 	}
 
 	/**
-	 * @returns the value if the Result is Ok, or a default value called from a function.
+	 * Unwraps the Ok value or returns a default value from a function.
+	 * @param {function(E): T} defaultValue The function to provide the default value.
+	 * @returns {T} The Ok value or the default value.
 	 */
-	unwrapOrElse(defaultValue: (err: E) => T) {
+	unwrapOrElse(defaultValue: (err: E) => T): T {
 		return this.isOk() ? (this.value as T) : defaultValue(this.value as E);
 	}
 
 	/**
-	 * Maps a `Result<T, _>` to `Result<U, _>` by applying a function to a contained value, if it's Ok.
+	 * Maps the Ok value to a new Result by applying a function.
+	 * @template U Type of the new Ok value
+	 * @param {function(T): U} fn The function to apply to the Ok value.
+	 * @returns {Result<U, E>} A new Result with the mapped Ok value.
 	 */
 	map<U>(fn: (value: T) => U): Result<U, E> {
 		return this.isOk()
 			? Result.Ok(fn(this.value as T))
-			: // This is safe, because we know that the value is an error.
-			  (this as unknown as Result<U, E>);
+			: (this as unknown as Result<U, E>);
 	}
 
 	/**
-	 * Maps a `Result<_, E>` to `Result<_, F>` by applying a function to a contained error, if it's Err.
+	 * Maps the error value to a new Result by applying a function.
+	 * @template F Type of the new error
+	 * @param {function(E): U} fn The function to apply to the error value.
+	 * @returns {Result<T, F>} A new Result with the mapped error value.
 	 */
 	mapErr<F>(fn: (err: E) => F): Result<T, F> {
 		return this.isErr()
 			? Result.Err(fn(this.value as E))
-			: // This is safe, because we know that the value is Ok.
-			  (this as unknown as Result<T, F>);
+			: (this as unknown as Result<T, F>);
 	}
 }
 
+/**
+ * @alias Result.Ok
+ */
 export const Ok = Result.Ok;
+
+/**
+ * @alias Result.Err
+ */
 export const Err = Result.Err;
 
-// Type aliases for convenience.
+// Type aliases
 
 export type PromiseResult<T, E> = Promise<Result<T, E>>;
 export type PResult<T, E> = PromiseResult<T, E>;
